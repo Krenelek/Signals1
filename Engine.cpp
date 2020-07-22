ac5102c273747672bab1f6c4feaff297123b40fc
Krenelek <lenrek.mada@gmail.com>
2020-07-22 19:32:00 +0200

    circular_convolution finished filter_generator in work

--- Engine.cpp
+++ Engine.cpp
@@ -61,25 +61,26 @@ using namespace std;
 		if (size_signal > size_filter)
 		{
 			shorter = size_filter;
+			filter.resize(size_signal,0);
 		}
 		else
 		{
 			shorter = size_signal;
+			signal.resize(size_signal, 0);
 		}
-		int counter = 0;
-		vector<double> output = vector<double>(size_signal + size_filter - 1,0);
+		vector<double> output = vector<double>(signal.size(), 0);
 		
 		for (int i = 0; i < output.size(); i++)
 		{
-			
-			if (i<shorter) { counter++; }
-			if (i > output.size()-shorter-1) { counter--; }
-			for (int j = 0; j < counter; j++)
+			for (int j = 0; j < i+1; j++)
 			{
 				output[i] = output[i] + filter[j] * signal[i-j];
-				cout << output[i] << endl;
 			}
-			cout << endl;
+			for (int j = i+1; j < output.size(); j++)
+			{
+				output[i] = output[i] + filter[j] * signal[output.size()+i-j];
+			}
+			
 		}
 		return output;
 	}
@@ -93,5 +94,51 @@ using namespace std;
 
 		return log(sum);
 	}
-	vector<double> Engine::
+	vector<double> Engine::circular_convolution(vector<double> signal, vector<double> filter)
+	{
+		int size_filter = filter.size();
+		int size_signal = signal.size();
+		int shorter = 0;
+		if (size_signal > size_filter)
+		{
+			shorter = size_filter;
+		}
+		else
+		{
+			shorter = size_signal;
+		}
+		int counter = 0;
+		vector<double> output = vector<double>(size_signal + size_filter - 1, 0);
+
+		for (int i = 0; i < output.size(); i++)
+		{
+
+			if (i < shorter) { counter++; }
+			if (i > output.size() - shorter - 1) { counter--; }
+			for (int j = 0; j < counter; j++)
+			{
+				output[i] = output[i] + filter[j] * signal[i - j];
+				cout << output[i] << endl;
+			}
+			cout << endl;
+		}
+		return output;
+	}
+	vector<double> Engine::filter_generator(int f1, int f2)
+	{
+		//prewarping
+		//bandpass
+		//-3dB bandwidth
+
+	}
+	vector<double> Engine::third_freq_generator(int octaves)
+	{
+		vector<double> output = vector<double>(octaves*3+1, 16);
+		double a = pow(2, (1 / 4));
+		for (int i = 0; i < octaves* 3; i++)
+		{
+			output[i + 1] = output[i] * a;
+		}
+		return output;
+	}
 
--- Engine.h
+++ Engine.h
@@ -11,9 +11,10 @@ public:
 	Engine(int _size);
 	void show_matrix_d();
 	vector<double> convolution(vector<double> signal, vector<double> filter);
-	double energy_log_scale(vector<double> signal);
+	vector<double> circular_convolution(vector<double> signal, vector<double> filter);
+	double energy_log_scale(vector<double> signal);//nie wiem czy potrzebny bo WAV jest w skali liniowej, ale to nie znaczy ¿e wyra¿a ju¿ poziom cisnienia
 	vector<double> filter_generator(int f1, int f2);
-	vector<int> third_freq_generator();
+	vector<double> third_freq_generator(int octaves);
 private:
 	const double pi2 = 4 * acos(0.0);
 	vector<double> tab;
