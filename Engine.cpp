2f439b30065227034634093cb8d031f66f065393
Krenelek <lenrek.mada@gmail.com>
2020-07-24 21:11:24 +0200

    repaired fft and also energy_in_thirds_log_scale

--- Engine.cpp
+++ Engine.cpp
@@ -1,5 +1,5 @@
-#include "Engine.h"
 using namespace std;
+#include "Engine.h"
 #include <iostream>
 #include <complex>
 #include <vector>
@@ -19,10 +19,36 @@ using namespace std;
 		}
 		cout << "engine has been created" << endl;
 	}
+	vector<complex<double>> Engine:: even_odd_decomposer(vector<complex<double>> input)
+	{
+		vector<vector<complex<double>>> output = vector<vector<complex<double>>>(2);
+		output[0] = vector<complex<double>>(input.size() / 2,0);
+		output[1]= vector<complex<double>>(input.size() / 2, 0);
+		for (int i = 0; i < input.size() / 2; i = i + 2)
+		{
+			output[0][i] = input[i];
+			output[1][i] = input[i + 1];
+		}
+		if (input.size() > 2)
+		{
+			output[0] = even_odd_decomposer(output[0]);
+			output[1] = even_odd_decomposer(output[1]);
+		}
+		vector<complex<double>> output2 = vector<complex<double>>(input.size());
+		for (int i = 0; i < output[1].size(); i++)
+		{
+			output2[i] = output[0][i];
+			output2[i + output2.size() / 2] = output[1][i];
+				
+		}
+		return output2;
+		
 
+	}
 	vector<complex<double>> Engine::fft(vector<complex<double>> input)
 	{
 		int a = input.size();
+		//tu ma byc uzupelnienie do 2^n
 		int b = 0;
 		while (a % 2 == 0)
 		{
@@ -30,14 +56,11 @@ using namespace std;
 			a = a / 2;
 			
 		}
-		
 		vector <complex<double>> output1, output2;
 		output2 = vector<complex<double>>(input.size(), 0);
-		int step = a;
-		for (int i = 0; i < input.size(); i++)
-		{
-			output2[i] = input[i];
-		}
+		output2 = even_odd_decomposer(input);		//zamiana 001 100
+
+		int step = a;	
 		while(step <input.size())
 		{
 			output1 = vector<complex<double>>(step, 0);
@@ -47,7 +70,7 @@ using namespace std;
 				{
 					output1[j] = output2[i + j];
 				}
-				output1 = dft(output1);
+				output1 = sub_fft(output1);			//butterfly krok
 				for (int j = 0; j < output1.size(); j++)
 				{
 					output2[i + j] = output1[j];
@@ -55,8 +78,54 @@ using namespace std;
 			}
 			step = step * 2;
 		}
-		//zamiana 001 100
 		return output2;
+	}
+	vector<complex<double>> Engine::ifft(vector<complex<double>> input)
+	{
+		int a = input.size();
+		//tu ma byc uzupelnienie do 2^n
+		int b = 0;
+		while (a % 2 == 0)
+		{
+			b++;
+			a = a / 2;
+
+		}
+		vector <complex<double>> output1, output2;
+		output2 = vector<complex<double>>(input.size(), 0);
+		output2 = even_odd_decomposer(input);		//zamiana 001 100
+
+		int step = a;
+		while (step < input.size())
+		{
+			output1 = vector<complex<double>>(step, 0);
+			for (int i = 0; i < input.size() / step; i = i + step)
+			{
+				for (int j = 0; j < step; j++)
+				{
+					output1[j] = output2[i + j];
+				}
+				output1 = sub_fft(output1);			//butterfly krok
+				for (int j = 0; j < output1.size(); j++)
+				{
+					output2[i + j] = output1[j];
+				}
+			}
+			step = step * 2;
+		}
+		return output2;
+	}
+	//do zmiany z fft
+
+	vector<complex<double>> Engine::sub_fft(vector<complex<double>> input)
+	{
+		int l = input.size() / 2;
+		vector<complex<double>> output = vector<complex<double>>(input.size(), 0);
+		for (int i = 0; i < l; i++)
+		{
+			output[i] = input[i] + input[i + l]* polar(1.0, pi2 * i / size);
+			output[i + l] = input[i] * polar(1.0, (pi2 * (i+l)) / size);
+		}
 	}
 	vector<complex<double>> Engine::idft(vector<complex<double>> input)
 	{
@@ -79,6 +148,7 @@ using namespace std;
 		return complexoutput;
 
 	}
+	// amoze by tak wykorzystac symetrie widma w srodku i liczyc tylko pol?
 	void Engine::show_matrix_d()
 	{
 		for (int i = 0; i < size; i++)
@@ -90,8 +160,6 @@ using namespace std;
 			cout << endl << endl;
 		}
 	}
-	//jak to jest z filtrami i ch dlugosciami w czestotliwosci
-	//metoda okien czasowych srodkowoprzepustowy
 
 	vector<double> Engine::convolution(vector<double> signal, vector<double> filter)
 	{
@@ -124,15 +192,30 @@ using namespace std;
 		}
 		return output;
 	}
-	double Engine::energy_log_scale(vector<complex<double>> signal)
+	vector<double> Engine::energy_in_thirds_log_scale(vector<complex<double>> signal_f)
 	{
-		double sum = 0;
-		for (int i = 0; i < signal.size(); i++)
-		{
-			sum = sum + abs(signal[i]) * abs(signal[i]);
-		}
+		//powinno byc razy dwa bo tylko pol widma ale i tak bedzie skalowane potem jakos
 
-		return log(sum);
+		vector<double> freq = third_freq_generator(9);
+		vector<double> output = vector<double>(freq.size(),0);
+		double step = 44100 / signal_f.size();
+		vector<int> poz = vector<int>(freq.size());
+		int k = 0;
+		for (int i = 0; i < freq.size();i++)
+		{
+			k=freq[i] / step;
+			freq[i] = k; //znalezienie najblizszego k w widmie (indeksu odpowiadajacego f);
+		}
+		for (int j = 0; j < freq.size()-1; j++)
+		{
+			for (int i = freq[j]; i < freq[j+1]; i++)
+			{
+				output[j] = output[j] + abs(signal_f[i]) * abs(signal_f[i]);
+			}
+
+			output[j] =log(output[j]);
+
+		}
 	}
 	vector<double> Engine::circular_convolution(vector<double> signal, vector<double> filter)
 	{
@@ -164,16 +247,9 @@ using namespace std;
 		}
 		return output;
 	}
-	vector<double> Engine::filter_generator(int f1, int f2)
-	{
-		//prewarping
-		//bandpass
-		//-3dB bandwidth
-
-	}
 	vector<double> Engine::third_freq_generator(int octaves)
 	{
-		vector<double> output = vector<double>(octaves*3+1, 16);
+		vector<double> output = vector<double>(octaves*3+1, 261.63/16);
 		double a = pow(2, (1 / 4));
 		for (int i = 0; i < octaves* 3; i++)
 		{
--- Engine.h
+++ Engine.h
@@ -8,23 +8,23 @@ class Engine
 {
 public:
 	vector<complex<double>> fft(vector<complex<double>> input);	
-	//juz dla wszystkich
-	//poprawka jest prosta - napisaæ j¹, do tego symetria binarna
+	// size!=2^n uzupe³nic poczatkiem
 	vector<complex<double>> ifft(vector<complex<double>> input);
-	//matrix inversion
+	//fft zmienic, dzielenie przez dlugosc
 	vector<complex<double>> dft(vector<complex<double>> input);
 	//potrzebna poprawka ze wzgledu na generowanie non stop macierzy przejscia niepotrzebnie, jakis if
 	vector<complex<double>> idft(vector<complex<double>> input);
+	//matrix inversion, dzielenie przez dlugosc
 	Engine(int _size);
 	void show_matrix_d();
 	vector<double> convolution(vector<double> signal, vector<double> filter);
 	vector<double> circular_convolution(vector<double> signal, vector<double> filter);
-	double energy_log_scale(vector<complex<double>> signal);
+	vector<double> energy_in_thirds_log_scale(vector<complex<double>> signal);
 	//nie wiem czy potrzebny bo WAV jest w skali liniowej, ale to nie znaczy ¿e wyra¿a ju¿ poziom cisnienia
-	vector<double> filter_generator(int f1, int f2);
 	vector<double> third_freq_generator(int octaves);
-	//down & up sampling
 private:
+	vector<complex<double>> even_odd_decomposer(vector<complex<double>> input);
+	vector<complex<double>> sub_fft(vector<complex<double>> input);
 	const double pi2 = 4 * acos(0.0);
 	vector<double> tab;
 	int size;
